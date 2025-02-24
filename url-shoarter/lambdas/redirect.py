bf5641dfc6f7ffb7c2ac63292101d8634620d643
MonyaS <salabaymisha@gmail.com>
2025-02-25 00:11:44 +0200

    last fixes

--- url-shoarter/lambdas/redirect.py
+++ url-shoarter/lambdas/redirect.py
@@ -15,22 +15,35 @@ def lambda_handler(event, context):
     if "Item" not in response:
         return {"statusCode": 404, "body": json.dumps({"error": "Not found"})}
 
-    record = short_code["Item"]
+    record = response["Item"]
 
     # Check if url has reached max redirections
-    if record.get("attempts") and record.get("attempts") <= 0:
-        return {
-            "statusCode": 400,
-            "body": json.dumps({"error": "Max redirect attempts reached"}),
-        }
+    if "attempts" in record.keys():
+        if record.get("attempts") <= 0:
+            return {
+                "statusCode": 400,
+                "body": json.dumps({"error": "Max redirect attempts reached"}),
+            }
+        else:
+            table.update_item(
+                Key={"short_code": event["pathParameters"]["short_code"]},
+                ExpressionAttributeNames={
+                    "#attempts": "attempts",
+                },
+                ExpressionAttributeValues={
+                    ":attempts": record.get("attempts") - 1,
+                },
+                UpdateExpression="SET #attempts = :attempts",
+                ReturnValues="ALL_NEW",
+            )
 
     # Deadline when link must be inactive
-    if record.get("end_date") and record.get("end_date") >= int(time.time() * 1000):
+    if record.get("end_date") and record.get("end_date") <= int(time.time() * 1000):
         return {"statusCode": 400, "body": json.dumps({"error": "Link expired"})}
 
     # update the todo in the database
     table.update_item(
-        Key={"id": event["pathParameters"]["id"]},
+        Key={"short_code": event["pathParameters"]["short_code"]},
         ExpressionAttributeNames={
             "#last_use": "last_use",
         },
--- url-shoarter/url_shoarter/url_shoarter_stack.py
+++ url-shoarter/url_shoarter/url_shoarter_stack.py
@@ -4,7 +4,9 @@ from aws_cdk import (
     aws_lambda as _lambda,
     aws_apigateway as apigw,
     aws_dynamodb as ddb,
+    RemovalPolicy
 )
+
 from constructs import Construct
 
 
@@ -18,7 +20,7 @@ class UrlShoarterStack(Stack):
             self,
             "UrlsTable",
             partition_key={"name": "short_code", "type": ddb.AttributeType.STRING},
-            removal_policy=ddb.RemovalPolicy.DESTROY,
+            removal_policy=RemovalPolicy.DESTROY,
         )
 
         # Shorten URL Lambda
@@ -26,8 +28,8 @@ class UrlShoarterStack(Stack):
             self,
             "ShortenLambdaSalabai",
             runtime=_lambda.Runtime.PYTHON_3_10,
-            handler="lambdas.shortener.lambda_handler",
-            code=_lambda.Code.from_asset("lambda"),
+            handler="shortener.lambda_handler",
+            code=_lambda.Code.from_asset("lambdas"),
             environment={"TABLE_NAME": table.table_name},
         )
 
@@ -36,14 +38,25 @@ class UrlShoarterStack(Stack):
             self,
             "RedirectLambdaSalabai",
             runtime=_lambda.Runtime.PYTHON_3_10,
-            handler="lambdas.redirect.lambda_handler",
-            code=_lambda.Code.from_asset("lambda"),
+            handler="redirect.lambda_handler",
+            code=_lambda.Code.from_asset("lambdas"),
+            environment={"TABLE_NAME": table.table_name},
+        )
+
+        # Info Lambda
+        info_lambda = _lambda.Function(
+            self,
+            "InfoLambdaSalabai",
+            runtime=_lambda.Runtime.PYTHON_3_10,
+            handler="info.lambda_handler",
+            code=_lambda.Code.from_asset("lambdas"),
             environment={"TABLE_NAME": table.table_name},
         )
 
         # Grant permissions to both Lambdas
         table.grant_read_write_data(shorten_lambda)
-        table.grant_read_data(redirect_lambda)
+        table.grant_read_write_data(redirect_lambda)
+        table.grant_read_data(info_lambda)
 
         # API Gateway
         api = apigw.RestApi(
@@ -59,3 +72,6 @@ class UrlShoarterStack(Stack):
         # Redirect endpoint
         redirect_resource = api.root.add_resource("{short_code}")
         redirect_resource.add_method("GET", apigw.LambdaIntegration(redirect_lambda))
+
+        # Info endpoint
+        redirect_resource.add_method("POST", apigw.LambdaIntegration(info_lambda))
